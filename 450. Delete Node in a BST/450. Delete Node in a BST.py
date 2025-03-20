c7d395972feaf1477cbe7cd1a1ac8e579ec8f022
EE002 <calice.liang@hyweb.com.tw>
2025-03-20 13:24:22 +0800

    Time: 0 ms (100%), Space: 21.5 MB (5%)

---
+++ 450. Delete Node in a BST/450. Delete Node in a BST.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Time: 0 ms (100%), Space: 21.5 MB (5%)
+class Solution(object):
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return None  
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left and not root.right:
+                return None
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+            
+            root.val = self.findMin(root.right)
+            root.right = self.deleteNode(root.right, root.val)
+
+        return root
+
+    def findMin(self, root: TreeNode) -> int:
+        while root.left:
+            root = root.left
+        return root.val
