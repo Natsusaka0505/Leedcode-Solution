f5dc77ae659c9d0948d6e71215b5446aec65aac3
EE002 <calice.liang@hyweb.com.tw>
2025-03-25 13:32:16 +0800

    update

--- 547. Number of Provinces/547. Number of Provinces.py
+++ 547. Number of Provinces/547. Number of Provinces.py
@@ -1,32 +1,28 @@
 from typing import List
-
+from collections import deque
 
 # Time: 3 ms (89.99%), Space: 19.36 MB (25.00%)
-from collections import deque
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        m,n = len(isConnected),len(isConnected[0])
-        adjList = [[] for _ in range(n)]
-        for i in range(m):
-            for j in range(n):
-                if isConnected[i][j]==1 and i!=j:
-                    adjList[i].append(j)
-                    adjList[j].append(i)
-        vis = [False]*n
-        res = 0
+        def dfs(node,d,v):
+            v[node]=True
+            for i in d[node]:
+                if (not v[i]):
+                    dfs(i,d,v)
+        n=len(isConnected)
+        d = {i: [] for i in range(n)}
         for i in range(n):
-            if not vis[i]:
-                qu = deque()
-                qu.append(i)
-                vis[i] = True
-                while qu:
-                    node = qu.popleft()
-                    for adj in adjList[node]:
-                        if not vis[adj]:
-                            vis[adj] = True
-                            qu.append(adj)
-                res+=1
-        return res
+            for j in range(n):
+                if isConnected[i][j]==1 and i!=j:               
+                    d[i].append(j)
+                    d[j].append(i)
+        v=[False]*n
+        ans=0
+        for i in range(n):
+            if (not v[i]):
+                ans += 1 
+                dfs(i,d,v)
+        return ans
 
 sol = Solution()
 inputList = [[1,1,0],[1,1,0],[0,0,1]]
