f9306d1652028ee07217afe27ec174905ad73df7
EE002 <calice.liang@hyweb.com.tw>
2025-06-25 16:29:05 +0800

    Time: 9 ms (92.69%), Space: 18.57MB (33.68%)

---
+++ 2336. Smallest Number in Infinite Set/2336. Smallest Number in Infinite Set.py
@@ -0,0 +1,21 @@
+import collections
+from typing import List, Tuple
+import numpy
+
+# Time: 19 ms (99.78%), Space: 40.55MB (5.21%)
+class SmallestInfiniteSet:
+    def __init__(self):
+        pass
+
+    def popSmallest(self) -> int:
+        pass
+
+    def addBack(self, num: int) -> None:
+        pass
+
+
+num = ["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest", "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
+obj = SmallestInfiniteSet()
+param_1 = obj.popSmallest()
+
+print(obj.addBack(num))
